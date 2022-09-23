10171d378a9638c7f470cb76a6ad4ee2291b9044
songtianyi <tianyi@saas3.io>
2022-09-23 16:57:02 +0800

    add log

--- lib.rs
+++ lib.rs
@@ -1,5 +1,3 @@
-
-
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[macro_use]
@@ -12,11 +10,12 @@ use pink_extension as pink;
 mod dRuntime {
     use super::*;
     use alloc::string::String;
+    use pink::PinkEnvironment;
 
     use futures::Future;
     use hex_literal::hex;
     use pink::http_get;
-    use pink_web3::api::{Accounts, Eth, Namespace};
+    use pink_web3::api::{Eth, Namespace};
     use pink_web3::contract::{Contract, Options};
     use pink_web3::transports::{resolve_ready, PinkHttp};
     use pink_web3::types::TransactionParameters;
@@ -32,10 +31,6 @@ mod dRuntime {
             Eth::new(PinkHttp::new(self.url.clone()))
         }
 
-        fn accounts(&self) -> Accounts<PinkHttp> {
-            Accounts::new(PinkHttp::new(self.url.clone()))
-        }
-
         #[ink(constructor)]
         pub fn default() -> Self {
             Self {
@@ -43,27 +38,29 @@ mod dRuntime {
             }
         }
 
-
-        async fn fetch_contract_events(&self) -> pink_web3::contract::Result<String> {
-            let contract_address = "0xC50fC6Ef39f1436382051562edfe1b70Fb4262b6";
+        fn fetch_contract_events(&self) -> pink_web3::contract::Result<String> {
+            // let contract_address = "0xC50fC6Ef39f1436382051562edfe1b70Fb4262b6";
+            let contract_address = hex!("d028d24f16a8893bd078259d413372ac01580769").into();
             // let my_account: = hex!("d028d24f16a8893bd078259d413372ac01580769").into();
-            let addr = H160::from_slice(contract_address.as_bytes());
+            // let addr = H160::from_slice(contract_address.as_bytes());
             let contract = Contract::from_json(
                 self.eth(),
-                addr,
+                contract_address,
                 include_bytes!("contract/res/saas3.abi.json"),
             )?;
             // Filter for event in our contract
             let filter = FilterBuilder::default()
-                .address(vec![addr])
-                .topics(
-                    None,
-                    None,
-                    None,
-                    None,
-                )
+                .address(vec![contract_address])
+                .topics(None, None, None, None)
                 .build();
-            let log = self.eth().logs(filter).resolve().unwrap();
+            pink::debug!("fetching logs");
+            let log = self.eth().logs(filter).resolve().map_err(|e| {
+                pink::debug!("error: {:?}", e);
+                e
+            })?;
+            for l in log {
+                pink::debug!("log: {:?}", l);
+            }
             //let filter = self.etheth_filter().create_logs_filter(filter).await?;
             //let logs_stream = filter.stream(time::Duration::from_secs(1));
             //futures::pin_mut!(logs_stream);
@@ -75,9 +72,11 @@ mod dRuntime {
             Ok(String::from("Good"))
         }
 
-        #[pink(on_block_end)]
+        //#[pink(on_block_end)]
+        #[ink(message)]
         pub fn on_block_end(&self) {
             let block_num = self.env().block_number();
+            pink::debug!("block_num: {:?}", block_num);
             // retrieve events every 12*3 seconds
             if block_num % 3 != 0 {
                 return;
@@ -96,7 +95,8 @@ mod dRuntime {
         fn tx_works() {
             pink_extension_runtime::mock_ext::mock_all_ext();
             let web3 = Web3::default();
-            _ = web3.tx();
+            _ = web3.on_block_end();
+            assert_eq!(1, 2);
         }
     }
 }
